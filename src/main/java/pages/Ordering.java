6b5ebc5a447f2d1da5cb38f21e04811a47cc8426
pHnkvr <rezuqqqqq@gmail.com>
2024-06-12 19:49:26 +0300

    test commit

--- src/main/java/pages/Ordering.java
+++ src/main/java/pages/Ordering.java
@@ -10,6 +10,8 @@ public class Ordering extends BasePage{
     private final static class Locators {
         private static final By changeSorting = By.xpath("//span[@class='sort-by__current']");
         private static final By sortByList = By.xpath("//ul[@class='sort-by__list']");
+        private static By sortByOption(String optionTitle) {
+            return By.xpath("//li[@title='" + optionTitle + "']");
     }
 
   public Ordering changeSort(String sortOption){
